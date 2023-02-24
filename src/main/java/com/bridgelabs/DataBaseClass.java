86fa58f121c57a66b6166112e0b43fe90577af14
hariprasad154 <hariprasas154@gmail.com>
2023-02-24 14:42:48 +0530

    added new wmployee called as rajewari

--- src/main/java/com/bridgelabs/DataBaseClass.java
+++ src/main/java/com/bridgelabs/DataBaseClass.java
@@ -22,7 +22,7 @@ public class DataBaseClass {
             //fetch the data
             System.out.println("The result set - " + rs);
             while (rs.next()) {
-                String userData = rs.getInt(1)+" "+rs.getNString(2) + "  " + rs.getDate(3)+" "+ rs.getDouble(4 ) + " "+ rs.getString(5);
+                String userData = rs.getInt(1)+" "+rs.getNString(2) + "  " + rs.getDate(3)+" "+ rs.getDouble(4 ) + " "+ rs.getString(5) + " " + rs.getDouble(6) + " "+ rs.getDouble(7);
                 System.out.println(userData);
             }
             System.out.println("Closing point of the Fetch method");
@@ -81,14 +81,46 @@ public class DataBaseClass {
     }
 
 
+    public static void insertDataUsingPreparedStatment(){
+        try {
+            Connection connection =DriverManager.getConnection(url,userName,password);
+            // Statement st = connection.createStatement();
+            int id=6;
+            String name ="Rajeswari";
+            double salary = 45000;
+            Date startDate = new Date(2022,8,20);
+            String gender ="F";
+            Double Taxpay= salary*0.10;
+            Double basepay=salary-salary*0.10;
 
+            String quary =" insert into payroll_Employewage(id,name,startDate,salary,Gender,BasicPay,TaxPay) values(?,?,?,?,?,?,?)";
+            System.out.println("--------The quary point for the updation ------ \n");
+            PreparedStatement st = connection.prepareStatement(quary);
+            st.setInt(1,id);
+            st.setString(2,name);
+            st.setDate(3,startDate);
+            st.setString(5,gender);
+            st.setDouble(4,salary);
+            st.setDouble(7,Taxpay);
+            st.setDouble(6,basepay);
+
+            int count = st.executeUpdate();
+            System.out.println("The no row affected + "+ count);
+            st.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            System.out.println("error - "+ e);
+        }
+
+    }
 
 
     public static void main(String[] args) {
       //  fetchData("SELECT * FROM payroll_Employewage WHERE startDate BETWEEN CAST('2021-01-01'AS DATE) AND DATE(NOW())");
 //        set();
-
-        fetchMethods("SELECT SUM(salary) FROM payroll_Employewage WHERE Gender = 'F' ");
-//        fetchData("select * from payroll_Employewage");
+        insertDataUsingPreparedStatment();
+        //fetchMethods("SELECT SUM(salary) FROM payroll_Employewage WHERE Gender = 'F' ");
+        fetchData("select * from payroll_Employewage");
     }
 }

76875daf6923b44c9bc33eb62e258266f4225431
hariprasad154 <hariprasas154@gmail.com>
2023-02-24 15:01:56 +0530

    added the the JDBC: Transactions

--- src/main/java/com/bridgelabs/DataBaseClass.java
+++ src/main/java/com/bridgelabs/DataBaseClass.java
@@ -16,18 +16,19 @@ public class DataBaseClass {
         try {
             Connection connection =DriverManager.getConnection(url,userName,password);
             Statement st = connection.createStatement();
-            System.out.println("The fecting the data is started  \n");
-            ResultSet rs =st.executeQuery(quaryFetch);
-            //The result we want in the table formatte so we use RS
-            //fetch the data
-            System.out.println("The result set - " + rs);
-            while (rs.next()) {
-                String userData = rs.getInt(1)+" "+rs.getNString(2) + "  " + rs.getDate(3)+" "+ rs.getDouble(4 ) + " "+ rs.getString(5) + " " + rs.getDouble(6) + " "+ rs.getDouble(7);
-                System.out.println(userData);
-            }
-            System.out.println("Closing point of the Fetch method");
-            st.close();
-            connection.close();
+
+                System.out.println("The fecting the data is started  \n");
+                ResultSet rs = st.executeQuery(quaryFetch);
+                //The result we want in the table formatte so we use RS
+                //fetch the data
+                System.out.println("The result set - " + rs);
+                while (rs.next()) {
+                    String userData = rs.getInt(1) + " " + rs.getNString(2) + "  " + rs.getDate(3) + " " + rs.getDouble(4) + " " + rs.getString(5) + " " + rs.getDouble(6) + " " + rs.getDouble(7);
+                    System.out.println(userData);
+                }
+                System.out.println("Closing point of the Fetch method");
+                st.close();
+                connection.close();
         } catch (SQLException e) {
             System.out.println("The error at the Connection - " + e);
         }
@@ -48,7 +49,6 @@ public class DataBaseClass {
             System.out.println("The no row affected + "+ count);
             st.close();
             connection.close();
-
         } catch (SQLException e) {
             System.out.println("error - "+ e);
         }
@@ -83,39 +83,45 @@ public class DataBaseClass {
 
     public static void insertDataUsingPreparedStatment(){
         try {
-            Connection connection =DriverManager.getConnection(url,userName,password);
-            // Statement st = connection.createStatement();
-            int id=6;
-            String name ="Rajeswari";
-            double salary = 45000;
-            Date startDate = new Date(2022,8,20);
-            String gender ="F";
-            Double Taxpay= salary*0.10;
-            Double basepay=salary-salary*0.10;
 
-            String quary =" insert into payroll_Employewage(id,name,startDate,salary,Gender,BasicPay,TaxPay) values(?,?,?,?,?,?,?)";
-            System.out.println("--------The quary point for the updation ------ \n");
-            PreparedStatement st = connection.prepareStatement(quary);
-            st.setInt(1,id);
-            st.setString(2,name);
-            st.setDate(3,startDate);
-            st.setString(5,gender);
-            st.setDouble(4,salary);
-            st.setDouble(7,Taxpay);
-            st.setDouble(6,basepay);
+                Connection connection = DriverManager.getConnection(url, userName, password);
+                // Statement st = connection.createStatement();
+                try {
+                    connection.setAutoCommit(false);
+                int id = 7;
+                String name = "Rajeswari2";
+                double salary = 45000;
+                Date startDate = new Date(2022, 8, 20);
+                String gender = "F";
+                Double Taxpay = salary * 0.10;
+                Double basepay = salary - salary * 0.10;
 
-            int count = st.executeUpdate();
-            System.out.println("The no row affected + "+ count);
-            st.close();
-            connection.close();
+                String quary = " insert into payroll_Employewage(id,name,startDate,salary,Gender,BasicPay,TaxPay) values(?,?,?,?,?,?,?)";
+                System.out.println("--------The quary point for the updation ------ \n");
+                PreparedStatement st = connection.prepareStatement(quary);
+                st.setInt(1, id);
+                st.setString(2, name);
+                st.setDate(3, startDate);
+                st.setString(5, gender);
+                st.setDouble(4, salary);
+                st.setDouble(7, Taxpay);
+                st.setDouble(6, basepay);
 
+                int count = st.executeUpdate();
+                System.out.println("The no row affected + " + count);
+                st.close();
+                connection.commit();
+            } catch (Exception e) {
+                connection.rollback();
+            } finally {
+                if (connection != null) {
+                    connection.close();
+                }
+            }
         } catch (SQLException e) {
             System.out.println("error - "+ e);
         }
-
     }
-
-
     public static void main(String[] args) {
       //  fetchData("SELECT * FROM payroll_Employewage WHERE startDate BETWEEN CAST('2021-01-01'AS DATE) AND DATE(NOW())");
 //        set();

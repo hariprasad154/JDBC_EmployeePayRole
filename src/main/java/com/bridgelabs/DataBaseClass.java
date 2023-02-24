7136969ff35b341257875ae558c36e828eee2d8f
hariprasad154 <hariprasas154@gmail.com>
2023-02-24 11:03:10 +0530

    added

--- src/main/java/com/bridgelabs/DataBaseClass.java
+++ src/main/java/com/bridgelabs/DataBaseClass.java
@@ -9,29 +9,9 @@ public class DataBaseClass {
     static String jdbcURL = "jdbc:mysql://localhost:3306/jdbcdemo1";
     static String userName="root";
     static String password="Hariprasad@12";
-    static Connection connection;
 
-    public static void main(String[] args) {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            System.out.println("Driver loaded!");
-        } catch (ClassNotFoundException e) {
-            throw new IllegalStateException("Cannot find the driver in the classpath!", e);
-        }
-        listDrivers();
-        try {
-            System.out.println("Connecting to database:" + jdbcURL);
-            connection = DriverManager.getConnection(jdbcURL, userName, password);
-            System.out.println("Connection is successful!!!!!!" + connection);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    private static void listDrivers () {
-        Enumeration<Driver> driverList = DriverManager.getDrivers();
-        while (driverList.hasMoreElements()) {
-            Driver driverClass = (Driver) driverList.nextElement();
-            System.out.println("  " + driverClass.getClass().getName());
-        }
-    }
+
+
+
+
 }
